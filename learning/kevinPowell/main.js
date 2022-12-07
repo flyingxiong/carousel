8ea8e712bbebd66e255262138199951f4da75f06
Shinyee Xiong <yingstudy@gmail.com>
2022-12-06 23:18:36 -0800

    finish main function, with bug in the first and last slides

---
+++ learning/kevinPowell/main.js
@@ -0,0 +1,63 @@
+const track = document.querySelector('.carousel__track');
+const slides = Array.from(track.children);
+const nextButton = document.querySelector('.carousel__button--right');
+const prevButton = document.querySelector('.carousel__button--left');
+const dotsNav = document.querySelector('.carousel__nav');
+const dots = Array.from(dotsNav.children);
+
+const slideWidth = slides[0].getBoundingClientRect().width;
+
+
+// arrange the slides next to one another
+const setSlidePosition = (slide, index) => {
+    slide.style.left = slideWidth * index + 'px'
+};
+
+slides.forEach(setSlidePosition);
+
+const moveToSlide = (track, currentSlide, targetSlide) => {
+    // move to the next slide
+    track.style.transform = 'translateX( -' + targetSlide.style.left +')';
+
+    currentSlide.classList.remove('current-slide');
+    targetSlide.classList.add('current-slide');
+};
+
+// when click left, move slides to the left
+nextButton.addEventListener('click', e => {
+    const currentSlide = track.querySelector('.current-slide');
+    const nextSlide = currentSlide.nextElementSibling;
+
+    moveToSlide(track, currentSlide, nextSlide);
+});
+
+
+
+// when clidk right, move slides to the right
+prevButton.addEventListener('click', e => {
+    const currentSlide = track.querySelector('.current-slide');
+    const prevSlide = currentSlide.previousElementSibling;
+
+    moveToSlide(track, currentSlide, prevSlide);
+});
+
+
+// when click the nav indicators, move to that slide
+dotsNav.addEventListener('click', e => {
+    // what indicator was clicked on?
+    const targetDot = e.target.closest('button');
+    // console.log('test1')
+    if (!targetDot) {return};
+    // console.log('test2')
+
+    const currentSlide = track.querySelector('.current-slide');
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const targetIndex = dots.findIndex(dot => dot === targetDot);
+    console.log(targetIndex);
+    const targetSlide = slides[targetIndex];
+    console.log(currentDot);
+    moveToSlide(track, currentSlide, targetSlide);
+    currentDot.classList.remove('current-slide');
+    targetDot.classList.add('current-slide');
+
+})
